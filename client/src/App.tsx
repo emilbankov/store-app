ee9e12e5926ba2ee0fbbef903acd1afba0459faa
emilbankov <emilbankov17d@gmail.com>
2025-04-02 22:51:23 +0300

    Update app component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,13 @@
+import Header from "./components/Header/Header"
+import Home from "./components/Home/Home"
+
 function App() {
 
     return (
-        <></>
+        <>
+            <Header />
+            <Home />
+        </>
     )
 }
 
