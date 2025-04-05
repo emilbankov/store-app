a2326bb8e8edaee6ae125aae3ea68c77fcd021e5
emilbankov <emilbankov17d@gmail.com>
2025-04-05 22:18:29 +0300

    Updated products services

--- client/src/services/productsService.ts
+++ client/src/services/productsService.ts
@@ -1,1 +1,33 @@
-const url = "https://localhost:8080/products";
+import axios from 'axios';
+
+const baseUrl = "http://localhost:8080/products"; 
+
+export const getProducts = async () => {
+    try {
+        const response = await axios.get(`${baseUrl}`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching products:', error);
+        throw error;
+    }
+};
+
+export const addProducts = async (productData: { name: string; price: number; quantity: number; unit: string, type: string, image: string }) => {
+    try {
+        const response = await axios.post(`${baseUrl}/add`, productData);
+        return response.data;
+    } catch (error) {
+        console.error('Error adding product:', error);
+        throw error;
+    }
+};
+
+export const removeProduct = async (id: string) => {
+    try {
+        const response = await axios.put(`${baseUrl}/${id}`);
+        return response.data;
+    } catch (error) {
+        console.error('Error removing product:', error);
+        throw error;
+    }
+};
