9372e077c9869f55b1dceb05fc0ec102118c77c9
emilbankov <emilbankov17d@gmail.com>
2025-04-14 16:58:29 +0300

    Added new request

--- client/src/services/purchasesService.ts
+++ client/src/services/purchasesService.ts
@@ -16,3 +16,13 @@ export const sellProducts = async (id: number, quantity: number) => {
     }
 };
 
+export const getPurchases = async () => {
+    try {
+        const response = await axios.get(baseUrl);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching purchases:', error);
+        throw error;
+    }
+};
+
