f25ab40a74d004074feb000cd439223d5e9a130a
emilbankov <emilbankov17d@gmail.com>
2025-04-14 22:06:45 +0300

    Created new service for reports

---
+++ client/src/services/reportService.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const baseUrl = "http://localhost:8080/report";
+
+export const report = async (startDate: string) => {
+    try {
+        const response = await axios.get(`${baseUrl}?startDate=${startDate}`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching purchases:', error);
+        throw error;
+    }
+};
